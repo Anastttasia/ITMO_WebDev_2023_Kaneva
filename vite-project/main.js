b8ff096845418a7e9998144b2fc99e2821a0588f
Anastttasia <nastya.kaneva.99@gmail.com>
2023-04-01 13:23:39 +0300

    JavaScript base

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,2 +1,29 @@
-import "uno.css";
-import '@unocss/reset/tailwind.css'
+const DOM = document.getElementById.bind(document);
+
+const domInpName =  DOM('inpName');
+const domInpSurname = DOM('inpSurname');
+const domConResult =  DOM('conResult');
+
+let fullName = "";
+
+domInpName.oninput = function(event){
+  console.log('onInpNameInput', {event});
+  renderFullName();
+}
+
+domInpSurname.oninput = function (event){
+  console.log('onInpSurnameInput', {event});
+  renderFullName();
+}
+
+const getFullName = () => `${domInpName.velue} ${domInpSurname.velue}`;
+
+function renderFullName(){
+  domConResult.textContent = getFullName();
+}
+
+
+
+
+
+console.log(domInpName, domInpSurname)
