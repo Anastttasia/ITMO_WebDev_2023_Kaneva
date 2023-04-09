1ce808a3ac7e1d4129cd26a520ed6d4e43be8787
Anastttasia <nastya.kaneva.99@gmail.com>
2023-04-09 15:28:36 +0300

    Continuation

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,29 +1,58 @@
+const appendBlock = (block) => document.getElementById("app").appendChild(block);
+const randomRange = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+const BLOCK_SIZE = 50;
+const STEP_DELTA_X = BLOCK_SIZE;
+const DIMENSION = 8;
+const DIMENSION_HALF = DIMENSION / 2;
+
 const DOM = document.getElementById.bind(document);
+const container = DOM("app")
 
-const domInpName =  DOM('inpName');
-const domInpSurname = DOM('inpSurname');
-const domConResult =  DOM('conResult');
+let columns = 0;
+let rows = 0;
+let xPos = 0;
+let yPos = 0;
 
-let fullName = "";
+let colorsInline = [];
 
-domInpName.oninput = function(event){
-  console.log('onInpNameInput', {event});
-  renderFullName();
+const getColorOrEmptyOnRandom  = () => {
+  const isNotEmpty= Math.random() > 0.5;
+  if (isNotEmpty){
+    return "black"
+  }
+  return null;
 }
 
-domInpSurname.oninput = function (event){
-  console.log('onInpSurnameInput', {event});
-  renderFullName();
+const createBLOCK = (x, y, size, color) => {
+  const result = document.createElement("div");
+  if(color){
+    result.style.backgroundColor = color;
+  }
+  result.style.width = result.style.height = `${size}px`;
+  result.style.position = "absolute";
+  result.style.color = "white";
+  result.style.left = `${x}px`;
+  result.style.top = `${y}px`;
+  return result;
 }
 
-const getFullName = () => `${domInpName.velue} ${domInpSurname.velue}`;
-
-function renderFullName(){
-  domConResult.textContent = getFullName();
+const saveColorForFutureUse = (color) => {
+  colorsInline.push(color);
 }
 
+while(columns < DIMENSION_HALF){
+  const color = getColorOrEmptyOnRandom();
+  const block = createBLOCK(xPos, yPos, BLOCK_SIZE, color);
+  saveColorForFutureUse(color);
+  appendBlock(block, container);
+  xPos += STEP_DELTA_X;
+  columns += 1;
+}
 
-
-
-
-console.log(domInpName, domInpSurname)
+const rigtHalfOffSet = (DIMENSION / 2) * BLOCK_SIZE;
+colorsInline.revers().forEach((color, BLOCK_SIZE) => {
+  const block = createBLOCK(BLOCK_SIZE * BLOCK_SIZE + rigtHalfOffSet);
+  appendBlock(block);
+  xPos += BLOCK_SIZE;
+});
