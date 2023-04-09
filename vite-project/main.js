94adc13c7e96eca67d8a59a64a6e275625a2c029
Anastttasia <nastya.kaneva.99@gmail.com>
2023-04-09 19:50:25 +0300

    Add javaScript

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,2 +1,15 @@
-import "uno.css";
-import '@unocss/reset/tailwind.css'
+// import "uno.css";
+import '@unocss/reset/tailwind.css';
+
+const domBtnCreateTask = document.getElementById("btnCreateTask");
+const domPopupCreateTask = document.getElementById("popupCreateTask");
+
+
+domBtnCreateTask.onclick = () => {
+  const domBtnCloseCreatTaskPopup = document.getElementById("btnCloseCreatTaskPopup");
+  domPopupCreateTask.classList.remove('hidden');
+  domBtnCloseCreatTaskPopup.onclick = () => {
+    domPopupCreateTask.classList.add('hidden');
+    omBtnCloseCreatTaskPopup.onclick = null;
+  }
+}
