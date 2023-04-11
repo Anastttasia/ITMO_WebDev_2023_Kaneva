0ecaf77da7f96c53d441b14926c85b8adba9e605
Anastttasia <nastya.kaneva.99@gmail.com>
2023-04-11 18:21:14 +0300

    Add open and close popup

---
+++ vite-project/src/const/DOM.js
@@ -0,0 +1,21 @@
+const Keys = {
+  DOM: {
+    Button: {
+      CREATE_TASK: "btnCreateTask",
+      CLOSE_CREATE_TASK_POPUP: "btnCloseCreatTaskPopup",
+      POPUP_CREATE_TASK_CONFIRM: "btnConfirmTaskPopup"
+    },
+    Template: {
+      TASK: "tempalateTask",
+      Task:{
+        TITLE: "tempalateTaskTitle",
+
+      } 
+    },
+      Popup: {
+      CREATE_TASK: "popupCreateTask"
+    },
+  },
+};
+
+export default Keys;
