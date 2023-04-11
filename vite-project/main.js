43dbfa286f79306f66d002832444a136f46980da
Anastttasia <nastya.kaneva.99@gmail.com>
2023-04-11 18:21:02 +0300

    Add open and close popup

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,15 +1,48 @@
 // import "uno.css";
-import '@unocss/reset/tailwind.css';
+// import '@unocss/reset/tailwind.css';
+import Keys from "./src/const/DOM";
+import { randomString } from "./src/const/utils/stringUtils";
 
-const domBtnCreateTask = document.getElementById("btnCreateTask");
-const domPopupCreateTask = document.getElementById("popupCreateTask");
+const DOM = (id) => document.getElementById(id);
+const QUERY = (container, id) => container.querySelector(`[data-id="${id}"]`);
 
+const domTask = DOM(Keys.DOM.Template.TASK)
+const tasks = [];
 
-domBtnCreateTask.onclick = () => {
-  const domBtnCloseCreatTaskPopup = document.getElementById("btnCloseCreatTaskPopup");
+const Tegs = ["Web", "Update", "Design", "Content"];
+class TaskVO {
+  constructor(title, date, status) {
+    this.title = title
+    this.date = date
+    this.status = status
+  }
+}
+
+DOM(Keys.DOM.Button.CREATE_TASK).onclick = () => {
+
+  const domPopupCreateTask = DOM(Keys.DOM.Popup.CREATE_TASK);
+  const domClosePopupCreateTask = QUERY(domPopupCreateTask, Keys.DOM.Button.CLOSE_CREATE_TASK_POPUP);
+  const domConfermPopupCreateTask = QUERY(domPopupCreateTask, Keys.DOM.Button.POPUP_CREATE_TASK_CONFIRM);
   domPopupCreateTask.classList.remove('hidden');
-  domBtnCloseCreatTaskPopup.onclick = () => {
+  
+  const onClosePopup = () => {
     domPopupCreateTask.classList.add('hidden');
-    omBtnCloseCreatTaskPopup.onclick = null;
+    domClosePopupCreateTask.onclick = null;
+    domConfermPopupCreateTask.onclick = null;
+  }
+
+  domClosePopupCreateTask.onclick = onClosePopup;
+
+  domConfermPopupCreateTask.onclick = () => {
+
+    const taskVO = new TaskVO(randomString(12), Date.now(), Tegs[0]);
+    const taskView = domTask.cloneNode(true);
+
+    QUERY(taskView, Keys.DOM.Template.Task.TITLE).innerText = taskVO.title;
+
+    domTask.parentNode.prepend(taskView);
+    tasks.push(taskVO)
+
+    onClosePopup();
   }
 }
