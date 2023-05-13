a78cbe6869338e62534550cbfa11d62e5df7709d
Anastttasia <nastya.kaneva.99@gmail.com>
2023-05-13 17:42:29 +0300

    Projects settings

---
+++ vite-project/src/components/MyName.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Enter you name</h1>
+  <div>
+    {{ firstName }}
+    <span style="color: red;">
+      {{ lastName }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyName",
+  data: () => ({
+    firstName: "Anastasia",
+    lastName: "Kaneva"
+  })
+};
+</script>
+
+<style scoped></style>
