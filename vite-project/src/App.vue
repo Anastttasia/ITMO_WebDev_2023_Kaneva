a0b22d5cc5e1322b14fab70f5811706c2ffc8a66
Anastttasia <nastya.kaneva.99@gmail.com>
2023-05-14 19:16:51 +0300

    Start Todo

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,42 +1,38 @@
-<script setup>
-import MyName from './components/MyName.vue';
-</script>
-
 <template>
-  <select name="status" 
-  @change="onSelectChenge"
+  <h3>Todo List</h3>
+  <input 
+  v-model="inputText"
+  @keyup.enter="canAddItemToTodoTheList && onInputEnterKeyUp()"
   >
-    <option v-if="!status" 
-    value="unselected" 
-    selected>
-      Unselected
-    </option>
-    <option v-for="item in statusList" 
-    :key="item" 
-    :value="item" 
-    :selected="item === status">
-      {{ item }}
-    </option>
-  </select>
-  <MyName 
-  :status="status" 
-  @reset="onStatusReset"/>
+  <div>
+    List:
+    <div 
+    v-for="item in todos" 
+    :key="item"
+    >
+    {{ item }}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    statusList: ['Ready', 'Start', 'Stop'],
-    status: 'Loading'
+    inputText: '',
+    todos: []
   }),
-  methods: {
-    onSelectChenge(event) {
-      console.log('> App -> onSelectChenge', event.target.value);
-      this.status = event.target.value;
+  computed:{
+    canAddItemToTodoTheList(){
+      console.log('> App -> canAddItemToTodoTheList');
+      return this.todoText?.length > 0;
     },
-    onStatusReset(){
-      console.log('> App -> onStatusReset');
-      this.status = null;
+    todoText(){return this.inputText?.trim(); }
+  },
+  methods: {
+    onInputEnterKeyUp(event) {
+      console.log('> App -> onInputEnterKeyUp');
+      this.todos.push(this.inputText.trim());
+      this.inputText = '';
     }
   }
 };
