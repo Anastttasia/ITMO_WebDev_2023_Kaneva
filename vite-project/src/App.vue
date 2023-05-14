fb303036f68238ed53b6d1ac2c59e38c7ac63633
Anastttasia <nastya.kaneva.99@gmail.com>
2023-05-14 18:21:42 +0300

    Basic MyName component

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,9 +1,45 @@
 <script setup>
-import MyName from "./components/MyName.vue";
+import MyName from './components/MyName.vue';
 </script>
 
 <template>
-  <MyName />
+  <select name="status" 
+  @change="onSelectChenge"
+  >
+    <option v-if="!status" 
+    value="unselected" 
+    selected>
+      Unselected
+    </option>
+    <option v-for="item in statusList" 
+    :key="item" 
+    :value="item" 
+    :selected="item === status">
+      {{ item }}
+    </option>
+  </select>
+  <MyName 
+  :status="status" 
+  @reset="onStatusReset"/>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    statusList: ['Ready', 'Start', 'Stop'],
+    status: 'Loading'
+  }),
+  methods: {
+    onSelectChenge(event) {
+      console.log('> App -> onSelectChenge', event.target.value);
+      this.status = event.target.value;
+    },
+    onStatusReset(){
+      console.log('> App -> onStatusReset');
+      this.status = null;
+    }
+  }
+};
+</script>
 
 <style scoped></style>
--- vite-project/src/components/MyName.vue
+++ vite-project/src/components/MyName.vue
@@ -2,20 +2,60 @@
   <h1>Enter you name</h1>
   <div>
     {{ firstName }}
-    <span style="color: red;">
+    <span style="">
       {{ lastName }}
     </span>
   </div>
+  <small>
+    <span :class="{
+      'selected': !!status,
+      'unselected': !status
+    }"
+    >
+    Status:</span>
+    <span>{{ status }}</span>
+    <div>
+      <button 
+      @click="onResetClick" 
+      :style="{
+        visibility: status ? 'visible' : 'hidden'
+      }"
+      >
+      Reset
+    </button>
+    </div>
+  </small>
 </template>
 
 <script>
 export default {
-  name: "MyName",
+  name: 'MyName',
+  props:{
+    status:{
+      type: String,
+      default: '-'
+    }
+  },
+  emits: ['reset'],
   data: () => ({
-    firstName: "Anastasia",
-    lastName: "Kaneva"
-  })
+    firstName: 'Anastasia',
+    lastName: 'Kaneva'
+  }),
+  methods: {
+    onResetClick(){
+      console.log('> MyName -> onResetClick');
+      this.$emit('reset');
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.selected {
+  color: green;
+}
+
+.unselected{
+  color: red;
+}
+</style>
