cc8210f22d0eef4ee845741960976013569da7a3
Anastttasia <nastya.kaneva.99@gmail.com>
2023-06-08 17:05:03 +0300

    Add couchdb

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -12,8 +12,25 @@ import PROVIDE from '@/constants/provides.js';
 const pb = new PocketBase('http://localhost:8090');
 console.log('pb.authStore.isValid:', pb.authStore.isValid);
 
-let db = new PouchDB('kittens');
-console.log(db);
+let PouchDB = require('pouchdb');
+let db = new PouchDB('http://127.0.0.1:5984/books');
+db.get('book1').then((doc) => {
+  console.log('doc', doc);
+});
+
+db.changes({
+  since: 'now',
+  live: true,
+  include_docs: true
+}).on('change', function(change) {
+  // handle change
+  console.log('change', change);
+}).on('complete', function(info) {
+  // changes() was canceled
+  console.log('complete', info);
+}).on('error', function (err) {
+  console.log(err);
+});
 
 createApp(AppComposition)
   .use(createPinia().use(piniaPluginPersistedState))
