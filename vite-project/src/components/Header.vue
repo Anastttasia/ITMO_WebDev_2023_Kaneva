12c8646c96b6f95386a7e0839464b6752aa9178a
Anastttasia <nastya.kaneva.99@gmail.com>
2023-07-04 14:33:18 +0300

    Add  vite, header

---
+++ vite-project/src/components/Header.vue
@@ -0,0 +1,60 @@
+<script setup>
+</script>
+
+<template>
+    <div class="header">
+      <div class="navigationMenu">
+        <h3>Products</h3>
+      </div>
+      <div class="slotHeader">
+        <span>Total:<p id=""></p></span>
+        <button class="btn">Card <p id=""></p></button>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+.header {
+  position: sticky;
+  top: 0;
+  left: 0;
+  background-color: rgba(190, 190, 190, 0.418);
+  max-width: 100vw;
+  height: 55px;
+  display: flex;
+  justify-content:space-between;
+  align-items: center;
+  z-index: 2;
+  margin-bottom: 60px;
+  padding: 0 1rem 0 1rem;
+  color: gray;
+  border-radius: 5px;
+}
+
+h3{
+  font-size: 20px;
+}
+.slotHeader{
+  display:flex;
+  flex-direction: row;
+  align-items: center;
+  font-weight: 700;
+  color: rgba(43, 41, 41, 0.658);
+}
+
+.btn{
+  height: 30px;
+  width: 5rem;
+  background-color: rgb(125, 199, 14);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 2rem;
+  font-weight: 500;
+  font-size: 1rem;
+  margin: 0 0.5rem 0 0.5rem;
+  color: white;
+  font-size: small;
+  margin-left: 5rem;
+}
+</style>
